a2d4ad5431a2e296393aa208b7234c03a3dab6bc
Shalom Abate <shaladi@mit.edu>
2016-10-24 09:48:26 -0400

    Delete entire old version

--- main/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import *
-
-# Register your models here.
-models = [Section, Lecturer, Material, Announcement, Course, Department, StudyField, Quote]
-
-for model in models:
-	admin.site.register(model)
