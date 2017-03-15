17413b58ed7b8beea9cbff95d17ba737e4964e62
genioaddis <genioaddis@gmail.com>
2017-03-15 13:26:05 +0300

    Turn on Debugging
    When debugging is off, its brings a 400 and then a 500 error

--- aaupush2016/settings.py
+++ aaupush2016/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '&t_qp-3u+aa!n1j-q8-d6blu(nup^h04_he^u*bm9lnviaj3dx'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
